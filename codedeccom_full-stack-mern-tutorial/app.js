998acf2cf66b4585f4452a368dbc60db1da61b2c
Sithembiso Maphanga <maphangasthembiso@gmail.com>
2022-08-29 18:35:15 +0200

    first commit

---
+++ codedeccom_full-stack-mern-tutorial/app.js
@@ -0,0 +1,77 @@
+const express               =  require('express'),
+      app                   =  express(),
+      mongoose              =  require("mongoose"),
+      passport              =  require("passport"),
+      bodyParser            =  require("body-parser"),
+      LocalStrategy         =  require("passport-local"),
+      passportLocalMongoose =  require("passport-local-mongoose"),
+      session               =  require("express-session"),
+      User                  =  require("./models/user");
+
+
+//Connecting database
+mongoose.connect("mongodb://localhost/auth_demo",{
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+});
+
+
+app.use(session({
+    secret:"Any normal Word",       //decode or encode session
+    resave: false,          
+    saveUninitialized:false,
+    cookie:{
+        maxAge: 2*60*1000 
+    }    
+}));
+
+
+passport.serializeUser(User.serializeUser());       //session encoding
+passport.deserializeUser(User.deserializeUser());   //session decoding
+passport.use(new LocalStrategy(User.authenticate()));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded(
+      { extended:true }
+))
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+//=======================
+//      R O U T E S
+//=======================
+app.get("/", (req,res) =>{
+    res.render("home");
+})
+
+//Auth Routes
+app.get("/register",(req,res)=>{
+    res.render("register");
+});
+
+
+
+app.post("/register",(req,res)=>{
+    User.register(new User({username: req.body.username,phone:req.body.phone}),req.body.password,function(err,user){
+        if(err){
+            console.log(err);
+            res.render("register");
+        }
+    passport.authenticate("local")(req,res,function(){
+        console.log("Following User has been registerd");
+        console.log(user)
+        res.redirect("/");
+    })    
+    })
+})
+
+
+
+app.listen(process.env.PORT ||3000,function (err) {
+    if(err){
+        console.log(err);
+    }else {
+        console.log("Server Started At Port 3000");
+    }
+      
+});
